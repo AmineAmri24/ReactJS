0e387dd43e3eabf69ee0bb9cca58efd8b4b59475
AmineAmri24 <amineamri2495@gmail.com>
2023-04-27 09:16:49 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,193 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Nav,Navbar,Container, Form}from 'react-bootstrap'
+import Carousel from 'react-bootstrap/Carousel';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* nav bar section */}
+
+
+      <Navbar bg="dark" variant="dark" sticky='top'>
+        <Container>
+          <Navbar.Brand href="#home">Pastry Art</Navbar.Brand>
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">recipes</Nav.Link>
+            <Nav.Link href="#pricing">share your receipe!</Nav.Link>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Form>
+        <div className='identifier'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      </div>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+      <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1587248721852-ffc60bffc129?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Welcome to our digital pastry</h3>
+          {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://plus.unsplash.com/premium_photo-1666353535506-36a287fd0423?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Welcome to our digital pastry</h3>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1593307744921-00b909a78419?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Welcome to our digital pastry</h3>
+          {/* <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p> */}
+
+
+          {/* Carousel section*/}
+
+
+
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+    {/* quick definition about the company objctf*/}
+
+
+
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Who are we ?</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Our objectif</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+
+
+    {/* sugested recipe*/}
+
+
+
+    <div className='cards'>
+    <Card className='total-card' style={{ width: '18rem' }}>
+      <Card.Img className='images' variant="top" src="https://images.unsplash.com/photo-1578775887804-699de7086ff9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2hlZXNlY2FrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+      <Card.Body>
+        <Card.Title>Cheesecake</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">discover more</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='total-card' style={{ width: '18rem' }}>
+      <Card.Img className='images' variant="top" src="https://images.unsplash.com/photo-1629324482344-58ac79e26b06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29vY2tpZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+      <Card.Body>
+        <Card.Title>Coockies</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">discover more</Button>
+      </Card.Body>
+    </Card>
+
+    <Card  className='total-card' style={{ width: '18rem' }}>
+      <Card.Img className='images' variant="top" src="https://images.unsplash.com/photo-1607920591413-4ec007e70023?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHRhcnRhJTIwZGUlMjBjaG9jb2xhdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+      <Card.Body>
+        <Card.Title>chocolate pie</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">discover more</Button>
+      </Card.Body>
+    </Card>
+    </div>
+
+
+    {/* footer and how to contact us */}
+      <footer>
+        <div className='footer'>
+        <h1>Feel free to contact us</h1>
+        <p>2023-2024 all right reserved</p>
+        </div>
+
+
+      </footer>
     </div>
   );
 }
